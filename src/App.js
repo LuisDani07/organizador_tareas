95439e65ade6f037f8fcacf66d95d86f32aad4a3
LuisDani07 <tics270118@gmail.com>
2022-10-20 19:56:07 -0600

    pasando el use state de App a Tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import Header from './components/Header'
-
-
+import Tasks from './components/Tasks'
+import {useState} from 'react';
 function App() {
+  const [tasks, setTask]=useState([{
+    id:1,
+    name:"lavar los trastes",
+    description:"lavar los trastes con agua y jabón ",
+    date: "february 5th at 3:pm",
+    reminder:true
+},
+{
+    id:2,
+    name:"leer un libro",
+    description:" leer un libro mientras voy en el camión",
+    date: "february 20th at 10:pm",
+    reminder:true
+},
+{
+    id:3,
+    name:"escribir en mi diario",
+    description: "escribir en mi diario todas las mañanas los ueños que hay tenidos, lo más detalladamente que se pueda",
+    date: "december 24th at 12:am",
+    reminder:false
+}])
 
   return (
        <div className="container">
           <Header />
+          <Tasks tasks={tasks}/>
        </div>
   )
 }
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,14 @@
+
+function Tasks({tasks}) {
+ 
+
+  return (
+    <>
+    {
+        tasks.map((task)=>(<h3 key={task.id}>{task.name}</h3>))
+    }
+     </>
+  )
+}
+
+export default Tasks
